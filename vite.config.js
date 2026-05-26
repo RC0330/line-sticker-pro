49f4a644217d3212a68291f0082fc56a24dd2062
RC0330 <rebecca@ctci.com>
2026-05-27 07:32:47 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,81 @@
 import {
   defineConfig
-}
-from "vite";
+} from "vite";
 
 import {
   VitePWA
-}
-from "vite-plugin-pwa";
+} from "vite-plugin-pwa";
 
 export default defineConfig({
+
+  build: {
+
+    chunkSizeWarningLimit: 20000,
+
+    rollupOptions: {
+
+      output: {
+
+        manualChunks: {
+
+          opencv: [
+            "@techstark/opencv-js"
+          ],
+
+          zip: [
+            "jszip",
+            "file-saver"
+          ]
+        }
+      }
+    }
+  },
 
   plugins: [
 
     VitePWA({
 
       registerType:
-        "autoUpdate"
+        "autoUpdate",
+
+      workbox: {
+
+        maximumFileSizeToCacheInBytes:
+          20 * 1024 * 1024
+      },
+
+      manifest: {
+
+        name:
+          "LINE Sticker AI PRO",
+
+        short_name:
+          "StickerAI",
+
+        theme_color:
+          "#0f172a",
+
+        background_color:
+          "#0f172a",
+
+        display:
+          "standalone",
+
+        icons: [
+
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
     })
   ]
 });
